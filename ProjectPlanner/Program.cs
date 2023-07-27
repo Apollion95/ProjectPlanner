122751700daa70e548de6280245c855a803379e0
Michał Szulc <54260979+Apollion95@users.noreply.github.com>
2023-07-27 16:56:32 +0200

    update and final

--- ProjectPlanner/Program.cs
+++ ProjectPlanner/Program.cs
@@ -1,32 +1,41 @@
-﻿using System;
-
-namespace ProjectPlanner
+﻿namespace ProjectPlanner
 {
     class Projekt
     {
-        public string Nazwa { get; set; }
-        public DateOnly ObecnyStart { get; set; }
-        public DateOnly ObecnyKoniec { get; set; }
+        public string Name { get; set; }
+        public DateOnly ExistingStart { get; set; }
+        public DateOnly ExistingEnd { get; set; }
         internal class Program
         {
             static void Main(string[] args)
             {
-               // int zgoda = 0; 
-                List<Projekt> obecne = new List<Projekt>();
-                obecne.Add(new Projekt { Nazwa = "Testowy1", ObecnyStart = new DateOnly(2020, 01, 02), ObecnyKoniec = new DateOnly(2020, 01, 06) });
-              //  obecne.Add(new Projekt { Nazwa = "Testowy2", ObecnyStart = new DateOnly(2020, 05, 10), ObecnyKoniec = new DateOnly(2020, 05, 15) });
-                DateOnly projektStart = new DateOnly(2020, 04, 8);
-                DateOnly projektKoniec = new DateOnly(2020, 04, 9);
-                int dlugosc = obecne.Count;
+                bool zgoda = false;
+                List<Projekt> ExistingProjects = new List<Projekt>();
+                ExistingProjects.Add(new Projekt { Name = "Signify", ExistingStart = new DateOnly(2020, 01, 03), ExistingEnd = new DateOnly(2020, 01, 30) });
+                ExistingProjects.Add(new Projekt { Name = "Tesco", ExistingStart = new DateOnly(2020, 02, 02), ExistingEnd = new DateOnly(2020, 02, 15) });
+                ExistingProjects.Add(new Projekt { Name = "Amazon", ExistingStart = new DateOnly(2020, 03, 01), ExistingEnd = new DateOnly(2020, 01, 15) });
+                DateOnly projectProposalStart = new DateOnly(2020, 02, 16);
+                DateOnly projectProposalEnd = new DateOnly(2020, 02, 29);
+                int dlugosc = ExistingProjects.Count;
+                //for (int i = 0; i < dlugosc; i++)
+                //{
+                //    if (projectProposalEnd < ExistingProjects[i].ExistingStart) Console.WriteLine("OK " + ExistingProjects[i].Name); 
+                //    else if (projectProposalStart > ExistingProjects[i].ExistingEnd) Console.WriteLine("OK " + ExistingProjects[i].Name);
+                //    else Console.WriteLine("Overlaps current project " + ExistingProjects[i].Name);
+                //}
                 for (int i = 0; i < dlugosc; i++)
                 {
-                    if (obecne[i].ObecnyStart < projektStart && obecne[i].ObecnyKoniec > projektKoniec) { Console.WriteLine("Poczatek i koniec nachodzi na obecny projekt " + obecne[i].Nazwa); }
-                    if (obecne[i].ObecnyStart > projektStart && obecne[i].ObecnyKoniec < projektKoniec) { Console.WriteLine("projekt nachodzi na obecny " + obecne[i].Nazwa); }
-                    if(obecne[i].ObecnyStart > projektStart && obecne[i].ObecnyKoniec > projektKoniec) { Console.WriteLine("Poczatek ok, koniec nachodzi na obecny projekt " + obecne[i].Nazwa); }
-                    if (obecne[i].ObecnyStart < projektStart && obecne[i].ObecnyKoniec < projektKoniec) { Console.WriteLine("Poczatek nachodzi na obecny projekt, koniec ok " + obecne[i].Nazwa); }
-                    if (obecne[i].ObecnyStart < projektKoniec || obecne[i].ObecnyKoniec < projektStart) { Console.WriteLine("zgoda  " + obecne[i].Nazwa); }
+                    if (projectProposalEnd < ExistingProjects[i].ExistingStart)
+                    {
+                        zgoda = true;
+                    }
+                    else if (projectProposalStart > ExistingProjects[i].ExistingEnd)
+                    {
+                        zgoda = true;
+                    }
+                    else zgoda = false;
                 }
-                
+                Console.WriteLine(zgoda);
             }
         }
     }
